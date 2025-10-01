eff106fb219dc80abde25a92bdce38056b8d575c
Yurii Hrytsai <rdestination@gmail.com>
2025-10-01 20:31:00 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# sbps-api
+# Spring Boot API Service
+REST API for business logic that works with sbps-device
+### RUN
---
+++ src/main/java/com/yh/sbps/api/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.yh.sbps.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "API Service is up!";
+    }
+
+}
