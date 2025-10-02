6ddb3905533a22ba5064bb98565a60aa190d7a82
Yurii Hrytsai <rdestination@gmail.com>
2025-10-03 00:33:30 +0300

    format

--- src/main/java/com/yh/sbps/api/controller/ApiController.java
+++ src/main/java/com/yh/sbps/api/controller/ApiController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "API Service is up!";
-    }
-
+  @GetMapping("/ping")
+  public String ping() {
+    return "API Service is up!";
+  }
 }
